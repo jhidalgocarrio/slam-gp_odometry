25bf06aa44b81a36d0b484973d820d91f2d98dfe
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2016-02-04 17:12:13 +0100

    src: update main

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,36 @@ int main()
 {
     gp_odometry::Sklearn gp;
 
-    gp.init("./data/gp_sklearn_x_delta_pose.data");
-    gp.getParams();
-    //std::vector<double> kernel_params = gp.kernel_();
-    //std::cout<<"Kernel parameters: ";
-    //for (std::vector<double>::const_iterator i = kernel_params.begin(); i != kernel_params.end(); ++i)
-    //{
-    //    std::cout << *i << ' ';
-    //}
-    //std::cout<<"\n";
+    gp.init("./data/gp_sklearn_x_delta_pose.data", "gp_x");
+    gp.getParams("gp_x");
+    std::vector<double> kernel_params = gp.theta_("gp_x");
+    std::cout<<"Kernel parameters: ";
+    for (std::vector<double>::const_iterator i = kernel_params.begin(); i != kernel_params.end(); ++i)
+    {
+        std::cout << *i << ' ';
+    }
+    std::cout<<"\n";
 
-     static const double arr[] = {9.93707217e-03,   1.01112053e-02,   9.95999202e-03,
-                         1.01173632e-02,   9.95666254e-03,   1.00015244e-02,
-                        -1.29177342e-05,  -7.75063527e-05,  -3.55237920e-04,
-                         4.52120694e-05,  -1.22847661e-04,  -5.16709370e-05,
-                        -2.04100197e-05,  -4.28868589e-05,  -7.63954769e-04,
-                        -2.97462996e-02,   1.04754493e-01,  -7.75860772e-02,
-                         5.71624078e-02,  -2.03379073e-03,   4.21097697e-04,
-                        -1.73008403e-03,  -1.05434945e-01,  -2.72711140e-01};
+    static const double arr[] = {3.54864560e-02,   3.48025449e-02,   3.48598845e-02,
+         3.42543200e-02,   3.43560986e-02,   3.38060819e-02,
+        -8.38438328e-03,  -1.46967657e-02,   8.38503335e-03,
+         1.47102010e-02,   0.00000000e+00,   0.00000000e+00,
+         0.00000000e+00,   5.16709349e-07,   0.00000000e+00,
+        -1.16259562e-05,   1.00195929e-02,   3.70861366e-02,
+        -7.05398992e-02,   1.56287136e-03,   1.10743524e-02,
+        -4.02362763e-03,   7.95247861e-02,   3.11162212e-02};
 
     std::vector<double> input_vector(arr, arr + sizeof(arr) / sizeof(arr[0]));
 
+    std::cout<<"Input vector: ";
+    for (std::vector<double>::const_iterator i = input_vector.begin(); i != input_vector.end(); ++i)
+    {
+        std::cout << *i << ' ';
+    }
+    std::cout<<"\n";
+
     double output_var = 0.00;
-    double output = gp.predict(input_vector, output_var);
+    double output = gp.predict("gp_x", input_vector, output_var);
     std::cout<<"Predict: "<<output<<" +-"<<output_var<<"\n";
 }
 
