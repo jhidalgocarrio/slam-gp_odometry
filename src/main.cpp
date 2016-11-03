424c87c6f15daa71b5342a103ed1bc801d48c1ec
Javier Hidalgo-Carrio <jhidalgocarrio@ieee.org>
2016-11-03 14:14:41 +0100

    scripts: moved methods to a new scripts folder

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main()
     /** Gaussian Process with Sklearn **/
     gp_odometry::Sklearn gp_sklearn;
 
-    gp_sklearn.init("./data");
+    gp_sklearn.init("./scripts");
     gp_sklearn.load("./data/gp_sklearn_x_delta_pose.data", "gp_x");
     gp_sklearn.getParams("gp_x");
     std::vector<double> kernel_params = gp_sklearn.theta_("gp_x");
@@ -42,7 +42,7 @@ int main()
     /** Gaussian Process with GPy **/
     gp_odometry::Gpy gp_gpy;
 
-    gp_gpy.init("./data");
+    gp_gpy.init("./scripts");
     gp_gpy.load("./data/SparseGP_RBF_xyz_velocities_train_at_500ms_normalized.data", "m");
     //gp_gpy.load("./data/SparseGP_RBF_xyz_velocities_train_at_1s_normalized.data", "m");
     //gp_gpy.load("./data/GP_RBF_xyz_velocities_train_at_1s_normalized.data", "m");
